7cc5d643d6092f3c12d40f40ffa73a67b142c308
Karsh Tharyani <karsht8@gmail.com>
2018-04-27 03:48:39 -0400

    trying to encapsulate

--- src/navigation_goals.cpp
+++ src/navigation_goals.cpp
@@ -1,19 +1,30 @@
 #include<ros/ros.h>
 #include<move_base_msgs/MoveBaseAction.h>
 #include<actionlib/client/simple_action_client.h>
+#include<nav_msgs/Path.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+typedef nav_msgs::Path Path;
+
+
+class listenPathAndPublishGoal
+{
+        private:
+                move_base_msgs::MoveBaseGoal goal;
+                ros::NodeHandle n;
+                ros::Subscriber pathListener; 
+        public:
+                listenPathAndPublishGoal(MoveBaseClient mb_client)
+                {
+                }
+};
 int main(int argc, char* argv[])
 {
         ros::init(argc, argv, "navigation_goals");
         MoveBaseClient ac("move_base", true);
-
-        while(!ac.waitForServer(ros::Duration(5.0)) && ros::ok())
-        {
+        while(!ac.waitForServer(ros::Duration(5.0))){
                 ROS_INFO("Waiting for the move_base action server to come up");
         }
-
-        move_base_msgs::MoveBaseGoal goal;
 
         while(ros::ok())
         {
