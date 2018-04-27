47f9f8b0cb306d10e3e3b6e192b7398746fd6d94
Karsh Tharyani <karsht8@gmail.com>
2018-04-27 04:21:57 -0400

    have an initial draft

--- src/navigation_goals.cpp
+++ src/navigation_goals.cpp
@@ -2,25 +2,33 @@
 #include<move_base_msgs/MoveBaseAction.h>
 #include<actionlib/client/simple_action_client.h>
 #include<nav_msgs/Path.h>
+#include<geometry_msgs/PoseStamped.h>
+#include<iostream>
 
+using namespace std;
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 typedef nav_msgs::Path Path;
+typedef geometry_msgs::PoseStamped PoseStamped;
 
+PoseStamped goalPoseInPath;
 
-class listenPathAndPublishGoal
+void pathCallBack(const Path::ConstPtr& googlePath)
 {
-        private:
-                move_base_msgs::MoveBaseGoal goal;
-                ros::NodeHandle n;
-                ros::Subscriber pathListener; 
-        public:
-                listenPathAndPublishGoal(MoveBaseClient mb_client)
-                {
-                }
-};
+        cout << googlePath->poses.size() << endl;
+        /*
+         *for(int i = 0; i < googlePath->poses.size(); ++i)
+         *{
+         *}
+         */
+}
+
 int main(int argc, char* argv[])
 {
         ros::init(argc, argv, "navigation_goals");
+        ros::NodeHandle nh;
+
+        ros::Subscriber pathListener = nh.subscribe("chatter1", 1, pathCallBack);
+
         MoveBaseClient ac("move_base", true);
         while(!ac.waitForServer(ros::Duration(5.0))){
                 ROS_INFO("Waiting for the move_base action server to come up");
